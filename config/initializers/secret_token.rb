577a22255efc03e3d69cdc95ff125072d2a6ef30
Epicodus <student@epicodus.com>
2013-04-03 09:41:46 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FictionGun::Application.config.secret_token = 'db95b797a8d23d543dd58a913a386f2024c8d9d09d864c102b52adcc7fe3a578b52e5c19bb5b28386760096dc09b1c30bc3f6c83117064e144550d3e7c1a734a'
