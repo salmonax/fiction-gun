8591fb24da0995583d76a93db4cc036ca860ddf8
George A. Michel & Dorrit Geshuri & Marcie Morton & Thomas Hopkins <student@epicodus.com>
2013-04-03 15:09:39 -0700

    fully implement and tests basic versions of stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,7 @@
 class StoriesController < ApplicationController
 
   def new
-    #@story = Story.new
+    @story = Story.new
   end
 
   def create
@@ -14,6 +14,35 @@ class StoriesController < ApplicationController
     else
       render :new
       flash[:alert] = "Please make sure all fields are filled in and try again."
+    end
+  end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def index
+    @stories = Story.all
+  end
+
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+    flash[:notice] = "Your story was successfully deleted."
+    redirect_to stories_path
+  end
+
+  def update
+    @story = Story.find(params[:id])
+    if @story.update_attributes(params[:story])
+      flash[:notice] = "Your contact was successfully updated! WOO!"
+      redirect_to story_path
+    else
+      render :edit
     end
   end
 
--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe StoriesController do
-
+  let(:story) {FactoryGirl.create :story}
   let(:valid_attributes) {{ :title => 'story of greatness', :text => 'one upon there was a dinosaur. then it died. the end.', :contributor_id => 1, :genre_id => 1}}
-  let(:valid_parameters) {{:story => valid_attributes}}
-
+  let(:valid_parameters) {{:story => valid_attributes, :id => story.id}}
+  let(:invalid_attributes) {{:title => ''}}
+  let(:invalid_parameters) {{:story => invalid_attributes, :id => story.id}}
 
   context 'routing' do
     it {should route(:get, '/stories/new').to :action => :new}
@@ -33,12 +34,52 @@ describe StoriesController do
     end
 
     context 'with invalid parameters' do
-      let(:invalid_attributes) {{:title => ''}}
-      let(:invalid_parameters) {{:story => invalid_attributes}}
       before {post :create, invalid_parameters}
 
       it {should render_template :new}
     end
   end
 
+  context 'GET index' do
+    before {get :index}
+
+    it {should render_template :index}
+  end
+
+  context 'PUT update' do
+    context 'with valid parameters' do
+      before {put :update, valid_parameters}
+
+      it 'updates the story' do
+        Story.find(story.id).title.should eq valid_attributes[:title]
+      end
+
+      it {should redirect_to story_path}
+      it {should set_the_flash[:notice]}
+    end
+
+    context 'with invalid parameters' do
+      before {put :update, invalid_parameters}
+
+      it {should render_template :edit}
+    end
+  end
+
+  context 'GET show' do
+    before {get :show, {:id => story.id}}
+
+    it {should render_template :show}
+  end
+
+  context 'DELETE destroy' do
+    it 'destroys a story' do
+      factory_story = FactoryGirl.create :story 
+      expect {delete :destroy, {:id => factory_story.id}}.to change(Story, :count).by(-1)
+    end
+
+    before {delete :destroy, {:id => story.id}}
+
+    it {should redirect_to stories_path}
+    it {should set_the_flash[:notice]}
+  end
 end
