d1f3d513eadb271432177fb431d2e6e67036770f
George A. Michel & Thomas Hopkins <student@epicodus.com>
2013-04-08 15:04:40 -0700

    initial commit dashboard model and controller with index view

---
+++ spec/models/dashboard_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Dashboard do
+
+  context '#stories' do
+    it 'displays the stories of the user who is logged in' do
+      user = FactoryGirl.create(:user)
+      story = FactoryGirl.create(:story, :users => [user])
+      dashboard = Dashboard.new(user)
+      dashboard.stories.should eq [story]
+    end
+  end
+
+  context '#user' do
+    it 'returns the current user' do
+      user = FactoryGirl.create(:user)
+      dashboard = Dashboard.new(user)
+      dashboard.user.should eq user
+    end
+  end
+end
